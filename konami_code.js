6d3f7ac1095fadf9a7f5cf724ae19889b2f84d12
rpyoungdale <youngdale95@gmail.com>
2018-04-29 19:11:12 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,19 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  return document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which);
 
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
     if (key === code[index]) {
       index++;
 
       if (index === code.length) {
         alert("Hurray!");
         index = 0;
-
-      } else {
-        index = 0;
       }
+    } else {
+      index = 0;
     }
   })
 }
